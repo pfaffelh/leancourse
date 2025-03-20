6046c02f22525c1e1c3cd498fd90e04090ddd6ed
pfaffelh <pfaffelh@gmail.com>
2025-03-20 23:39:36 +0100

    update1

--- Leancourse/Basic.lean
+++ Leancourse/Basic.lean
@@ -18,7 +18,21 @@ example : 2 + 2 = 4 :=
 authors := ["Peter Pfaffelhuber"]
 %%%
 
-These are some notes for the course on Lean at the University of Freiburg in Summer 2025.
+These are the notes for a course on formal proving with the interactive theorem prover Lean4 (in the following we just write Lean) in the summer semester of 2025 at the University of Freiburg. To be able to work through the course in a meaningful way, the following technical preparations are to be made:
+
+* Installation of [vscode](https://code.visualstudio.com/).
+* Local installation of Lean and the associated tools: Please follow these [instructions](https://github.com/leanprover-community/mathlib4/wiki/Using-mathlib4-as-a-dependency).
+* Installing the course repository: Navigate to a location where you would like to put the course materials and use `git clone get https://github.com/pfaffelh/leancourse`.
+* After `cd leancourse` and `code .`, you should see some code which looks a bit like mathematics. On the left hand side of the `vscode` window, you can click and install an extension for `Lean`.
+* The directory `Exercises` contains the material for the course. We recommend that you first copy this directory, for example to `myExercises`. Otherwise, an update of the repository may overwrite the local files.
+* To update the course materials, enter `git pull` from within the `leancourse`directory.
+
+# Introduction
+
+The course is designed for mathematics students and has at least two goals:
+
+* Learning the techniques for interactive formal Theorem proofing using Lean: In recent years, efforts to prove mathematical theorems with the help of computers have increased dramatically. While a few decades ago, it was more a matter of consistently processing many cases that were left to the computer, interactive theorem provers are different. Here, a very small core can be used to understand or interactively generate all the logical conclusions of a mathematical proof. The computer then reports interactively on the progress of the proof and when all the steps have been completed.
+* Establishing connections to some mathematical material: On the one hand, the mathematical details needed in this course should not be the main issue of this course. On the other hand, in order to _explain_ how a proof (or calculation or other argument) to a computer, you first have to understand it very well yourself. Furthermore, you have to plan the proof well -- at least if it exceeds a few lines -- so that the commands you enter (which we will call tactics) fit together.
 
 # First steps using logic
 
@@ -31,6 +45,13 @@ example : P → Q := by
   intro h
   sorry
 ```
+
+```lean
+example (hP : P) : (P → Q) → Q := by
+  intro hPQ
+  exact hPQ hP
+```
+
 
 ```
 Proof state   Command         New proof state
--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 To build the example website and place the results in _out/examples/demosite, run:
 
 lake build
-lake exe demosite --output _out/examples/demosite
+lake exe leancourse --output _out/
 To view the output, a local server will be needed. One way to get such a server is to use the one from the Python standard library, e.g.
 
-python3 -m http.server 8800 --directory _out/examples/demosite &
+python3 -m http.server 8800 --directory _out/html-single/ &
 after which http://localhost:8800/ will show the generated site.
 
 https://pfaffelh.github.io/leancourse/
+shows 
